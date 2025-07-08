520f74133329519d224c11117df5d9f6d7ef581c
Steven Chiu <stchiu@email.tamu.edu>
2025-07-07 22:44:45 -0700

    add nomad deeponet (#2)
    * add nomad deeponet
    
    * init exp

--- deeponets/models.py
+++ deeponets/models.py
@@ -5,6 +5,8 @@ import torch
 import torch.nn as nn
 from functorch import make_functional
 from .modules import MLP
+import warnings
+from functorch.experimental import replace_all_batch_norm_modules_
 
 class DeepONetBase(nn.Module):
     def __init__(self, n_branch, n_trunk, n_inter, n_output):
@@ -20,6 +22,7 @@ class DeepONetBase(nn.Module):
         self.n_trunk = n_trunk
         self.n_inter = n_inter
         self.n_output = n_output
+    
     def forward(self, x_branch:torch.Tensor, x_trunk:torch.Tensor):
         """
         x_branch: (n_batch, n_branch)
@@ -72,3 +75,44 @@ class DeepONet(DeepONetBase):
         out = torch.bmm(trunk_out, branch_out) + self.bias # (n_batch, n_query, 1)
         return out
 
+class NOMDADDeepONet(DeepONetBase):
+    """
+    DeepONet with NOMDAD (Nonlinear Manifold Decoders for Operator Learning)
+    Ref: 
+    1. Paper: https://openreview.net/pdf?id=5OWV-sZvMl
+    2. Code: https://github.com/PredictiveIntelligenceLab/NOMAD/blob/86ce2049dcc7446c356563f168f191c14f147e25/Antiderivative/Train_model/train_antiderivative.py#L112-L119
+    """
+    def __init__(self, n_branch:int, n_trunk:int, n_inter:int, hiddens_branch:list[int]=[], hiddens_trunk:list[int]=[], activation=nn.ReLU(),final_activation_branch=nn.Identity(), final_activation_trunk=nn.Identity(), ln=None, n_output=1):
+        super().__init__(n_branch, n_trunk, n_inter, n_output=n_output)
+        self.n_layers_branch = [n_branch, *hiddens_branch, n_inter]
+        self.n_layers_trunk = [n_inter + n_trunk, *hiddens_trunk, n_output]
+
+        self.branch = MLP(self.n_layers_branch, activation=activation, final_activation=final_activation_branch, ln=ln)
+        self.trunk = MLP(self.n_layers_trunk, activation=activation, final_activation=final_activation_trunk, ln=ln)
+        
+        # Batchnorm case is patched
+        if ln=="batchnorm":
+            warnings.warn("NOMADDeepONet does not support batch normalization because of vmap. Using layer normalization instead. Patch all batchnorm via https://docs.pytorch.org/functorch/stable/batch_norm.html")
+            self.trunk = replace_all_batch_norm_modules_(self.trunk)
+
+
+    def forward(self, x_branch:torch.Tensor, x_trunk:torch.Tensor):
+        """
+        x_branch: (n_batch, n_branch)
+        y_trunk: (n_batch, n_query, n_trunk)
+        """
+        if x_trunk.dim() == 2:
+            # If x_trunk is (n_batch, n_trunk), we need to unsqueeze it to (n_batch, 1, n_trunk). means 1 example
+            x_trunk = x_trunk.view(x_trunk.shape[0], 1, x_trunk.shape[1])
+
+        def nomad(br, tr):
+            """
+            br: branch input (n_batch, n_branch_out)
+            tr: trunk input (n_batch, n_trunk)
+            """
+            inp = torch.cat([br, tr], dim=-1) # (n_batch, n_branch_out + n_trunk)
+            return self.trunk(inp) # (n_batch, n_query, n_output)
+        br = self.branch(x_branch)  # (n_batch, n_branch_out)
+        out = torch.vmap(nomad, in_dims=(None, 1))(br, x_trunk) # (n_query, n_batch, n_output)
+        out = out.transpose(0,1)  # (n_batch, n_query, n_output)
+        return out#.transpose(2,1)
