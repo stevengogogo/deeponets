ac96e4d7a43d885397279da5179fc9453a24768d
Shao-Ting Chiu <stchiu@email.tamu.edu>
2025-07-08 01:08:30 -0500

    wip: unsqueeze data

--- exps/antiderivative.py
+++ exps/antiderivative.py
@@ -2,4 +2,26 @@
 import wget
 import os 
 import deeponets
+import numpy as np
+
+#%%
+wget.download("https://github.com/mroberto166/CAMLab-DLSCTutorials/raw/main/antiderivative_aligned_train.npz")
+wget.download("https://github.com/mroberto166/CAMLab-DLSCTutorials/raw/main/antiderivative_aligned_test.npz")
+
+
+# %%
+
+dataset_train = np.load("antiderivative_aligned_train.npz", allow_pickle=True)
+
+branch_train = dataset_train["X"][0]
+trunk_train = dataset_train["X"][1]
+outputs_train = dataset_train["y"]
+
+print('branch', branch_train.shape)
+print('trunk', trunk_train.shape)
+print('output', outputs_train.shape)  
+# %%
+
+trunk_train.unsqueeze(0).repeat(150).shape
+
 # %%
